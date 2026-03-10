2ce4b68e1fd99ee121ac037def0437691d5a181e
SergioAM25 <123046330@upq.edu.mx>
2026-03-10 15:39:29 -0600

    Último commit examen

--- Examen2doParcial/app/main.py
+++ Examen2doParcial/app/main.py
@@ -15,15 +15,44 @@ app = FastAPI(
     version= "1.0"
 )
 
+reservas = [
+    {"id": 1, "nombre": "Sergio Alvarez Matias", "edad": 21},
+    {"id": 2, "nombre": "Ali", "edad": 21},
+    {"id": 3, "nombre": "Dulce", "edad": 21},
+]
+
+#Modelo de datos para crear reserva
 class crear_reserva(BaseModel):
-    nombre: str = Field(..., ej="Sergio Alvarez Matias")
+    id: int = Field(..., ej="1")
+    nombre: str = Field(..., min_length=6,ej="Sergio Alvarez Matias")
     fecha: str = Field(..., ej="2026-01-01")
     hora: str = Field(..., ej="19:00")
     numero_personas: int = Field(..., ej=4)
 
-class reserva(crear_reserva):
-    id: int = Field(..., ej=1)
-    estado: str = Field(..., ej="pendiente")
+@app.get("/v1/reservas/", tags=['HTTP CRUD'])
+async def reservas():
+    return {"total": len(reservas), "reservas": reservas}
+
+@app.post("/v1/usuarios/", tags=['HTTP CRUD'], status_code=status.HTTP_201_CREATED)
+async def crear_reserva(usuario: crear_reserva):
+    for usr in reservas:
+        if usr ["id"] == usuario.id:
+            raise HTTPException(
+                status_code=400, 
+                detail="El id ya existe"
+            )
+    
+    reservas.append(usuario)
+    return {"mensaje": "Reserva Creada", "Reserva": usuario}
+
+#Modelo de datos para listar reservas
+class listar_reservas(BaseModel):
+    id: int = Field(..., ej="1"),
+    nombre: str = Field(..., ej="Sergio Alvarez Matias")
+    fecha: str = Field(..., ej="2026-10-03")
+    hora: str = Field(..., ej="02:55")
+    personas: int = Field(..., min_length=1, max_length=10, ej="2")
+    estado: str = Field(..., ej="Pendiente")
 
 security = HTTPBasic()
 def verificar_credenciales(credentials: HTTPBasicCredentials = Depends(security)):
@@ -36,15 +65,24 @@ def verificar_credenciales(credentials: HTTPBasicCredentials = Depends(security)
             headers={"WWW-Authenticate": "Basic"},
         )
 
-reservas = []
-id_reserva = 1
-
-class listar_reservas(BaseModel):
-    id: int
-    nombre: str
-    fecha: str
-    hora: str
-    personas: int
-    estado: str
-
+#Modelo de datos para cancelar reservas
+@app.delete("/v1/usuarios/{usuario_id}", tags=['HTTP CRUD'])
+async def cancelar_reserva(reserva_id: int):
+    "Elimina reserva de la lista."
+    for i, usr in enumerate(reservas):
+        if usr["id"] == reserva_id:
+            usuario_eliminado = reservas.pop(i)
+            return {"mensaje": "Reserva cancelada", "reserva": usuario_eliminado}
     
+    raise HTTPException(status_code=404, detail="Reserva no encontrada")
+
+security = HTTPBasic()
+def verificar_credenciales(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "admin")
+    correct_password = secrets.compare_digest(credentials.password, "rest123")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciales incorrectas",
+            headers={"WWW-Authenticate": "Basic"},
+        )
