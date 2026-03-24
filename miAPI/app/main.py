cec2b5e1613fce0f7375d08de6d14150bc2ed7e9
SergioAM25 <123046330@upq.edu.mx>
2026-03-24 14:56:15 -0600

    Practica 9- BD

--- miAPI/app/main.py
+++ miAPI/app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from app.data import usuario as usuarioDB
 from app.router import usuario,misc
+from app.data.db import engine
+
+usuarioDB.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title= "Mi Primer API",
--- miAPI/app/router/usuario.py
+++ miAPI/app/router/usuario.py
@@ -3,26 +3,35 @@ from app.models.usuario import crear_usuario
 from app.data.database import usuarios
 from app.security.auth import verificar_peticion
 
+from sqlalchemy.orm import Session
+from app.data.db import get_db
+from app.data.usuario import usuario as dbUsuario
+
 router= APIRouter(
     prefix="/v1/usuarios",
     tags=["HTTP CRUD"]
 )
 
 @router.get("/")
-async def leer_usuarios():
-    return {"total": len(usuarios), "usuarios": usuarios}
+async def leer_usuarios(db:Session = Depends(get_db)):
+    queryUsuarios = db.query(dbUsuario).all()
+    return {
+        "status": "200", 
+        "total": len(queryUsuarios), 
+        "usuarios": queryUsuarios
+        }
     
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def crear_usuario(usuario: crear_usuario):
-    for usr in usuarios:
-        if usr ["id"] == usuario.id:
-            raise HTTPException(
-                status_code=400, 
-                detail="El id ya existe"
-            )
-    
-    usuarios.append(usuario)
-    return {"mensaje": "Usuario Creado", "Usuario": usuario}
+async def crear_usuario(usuarioP: crear_usuario, db:Session = Depends(get_db)):
+    nuevoU = dbUsuario(nombre = usuarioP.nombre, edad = usuarioP.edad)
+    db.add(nuevoU)
+    db.commit()
+    db.refresh(nuevoU)
+
+    return {
+        "mensaje": "Usuario Creado", 
+        "Usuario": usuarioP
+        }
 
 #PUT, PATCH Y DELETE 
     
